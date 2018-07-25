6527acbd6ad3079053296fd2a8bb2d0f22f937bc
vanyakoval <37673292+vanyakoval@users.noreply.github.com>
2018-07-25 19:33:36 +0300

    Delete index.js.js

--- index.js.js
+++
@@ -1,7 +0,0 @@
-var http=require ('http')
-http.createServer(function(request,response) {
-    response.end("hello nodejs");
-})
-    .listen(3000,"127.0.0.1",function(){
-        console.log("Сервер начал прослушивать запросы на порту 3000");
-});
